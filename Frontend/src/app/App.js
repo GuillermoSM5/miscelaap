4d173e0f0d9f1ca74e5ec184b42db259f06501b3
GuillermoSM <guillermosotomarquez5@gmail.com>
2021-04-16 16:14:01 -0500

    branch memo

--- Frontend/src/app/App.js
+++ Frontend/src/app/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD:Frontend/src/app/App.js
-import '../scss/App.scss';
-=======
 import './App.css';
 import Layout from './components/Layout/Layout';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
->>>>>>> guillermoSoto:Frontend/src/App.js
 
 function App() {
 	return (
 		<Router>
 			<Layout>
 				<Switch>
-					<Route exact path="/" component={Home} />
+					<Route exact path='/' component={Home} />
 				</Switch>
 			</Layout>
 		</Router>
